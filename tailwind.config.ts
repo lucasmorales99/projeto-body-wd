0b392dbfdd25789ff7d558c6daed3e81875fe654
Santos <ictxdiogo@gmail.com>
2023-08-10 23:22:20 -0400

    feat: install prettier and fix files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ["./src/**/*.{ts,tsx}"],
   plugins: [],
-}
-export default config
+};
+export default config;
