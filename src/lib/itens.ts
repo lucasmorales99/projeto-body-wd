0aa7b26805e17950e834f40f56d8d51da57c63db
Santos <ictxdiogo@gmail.com>
2023-09-08 15:05:33 -0400

    fix: separado interface e itens em seus devidos arquivos

---
+++ src/lib/itens.ts
@@ -0,0 +1,55 @@
+import { v4 } from "uuid";
+
+import { Card } from "@/lib/interfaces";
+
+export const projectsCards: Card[] = [
+  {
+    id: v4(),
+    imageHref: "https://i.imgur.com/VyPhjnu.jpg",
+    description: "Descrição1",
+    title: "Titulo1",
+    pageHref: "/",
+  },
+  {
+    id: v4(),
+    imageHref: "https://i.imgur.com/VyPhjnu.jpg",
+    description: "Descrição2",
+    title: "Titulo2",
+    pageHref: "/",
+  },
+  {
+    id: v4(),
+    imageHref: "https://i.imgur.com/VyPhjnu.jpg",
+    description: "Descrição3",
+    title: "Titulo3",
+    pageHref: "/",
+  },
+  {
+    id: v4(),
+    imageHref: "https://i.imgur.com/VyPhjnu.jpg",
+    description: "Descrição3",
+    title: "Titulo3",
+    pageHref: "/",
+  },
+  {
+    id: v4(),
+    imageHref: "https://i.imgur.com/VyPhjnu.jpg",
+    description: "Descrição2",
+    title: "Titulo2",
+    pageHref: "/",
+  },
+  {
+    id: v4(),
+    imageHref: "https://i.imgur.com/VyPhjnu.jpg",
+    description: "Descrição3",
+    title: "Titulo3",
+    pageHref: "/",
+  },
+  {
+    id: v4(),
+    imageHref: "https://i.imgur.com/VyPhjnu.jpg",
+    description: "Descrição3",
+    title: "Titulo3",
+    pageHref: "/",
+  },
+];
