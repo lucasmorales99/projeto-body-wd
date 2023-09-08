cd41d1478e17436fa75384e076da9c05771cd72b
Santos <ictxdiogo@gmail.com>
2023-09-08 15:32:32 -0400

    fix: removendo main duplicada

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <NavBar title={"Portfolio"} menuItens={homeNavBarItems} />
-      <main>{children}</main>
+      {children}
     </>
   );
 }
